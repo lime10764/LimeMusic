b10cfdad75b70d6a6be0d2a0945bfeb2383e2309
lime10764 <lime10764@gmail.com>
2026-09-04 02:16:33 +0800

    Fix interaction source compilation

--- app/src/main/java/com/limemusic/app/ui/MainScreen.kt
+++ app/src/main/java/com/limemusic/app/ui/MainScreen.kt
@@ -20,6 +20,8 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
