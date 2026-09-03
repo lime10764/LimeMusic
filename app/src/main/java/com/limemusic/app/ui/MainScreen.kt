619cd35ef634cceb2c5acbbe8c129c2e1dd15e6e
lime10764 <lime10764@gmail.com>
2026-09-04 01:42:11 +0800

    Fix MainScreen Kotlin compilation errors

--- app/src/main/java/com/limemusic/app/ui/MainScreen.kt
+++ app/src/main/java/com/limemusic/app/ui/MainScreen.kt
@@ -487,7 +487,7 @@ private fun MusicList(
         ) { index, music ->
 
             MusicRow(
-                music = music,
+                currentMusic = music,
                 playing = currentMusic?.id == music.id,
                 onClick = {
                     onMusicClick(index)
@@ -576,7 +576,7 @@ private fun MusicRow(
                 )
 
                 Icon(
-                    imageVector = Icons.Default.GraphicEq,
+                    imageVector = Icons.Default.Equalizer,
                     contentDescription = "正在播放",
                     tint = Blue,
                     modifier = Modifier
@@ -748,7 +748,7 @@ private fun MiniPlayer(
                 ) {
                     Icon(
                         imageVector = if (isPlaying)
-                            Icons.Default.GraphicEq
+                            Icons.Default.Equalizer
                         else
                             Icons.Default.MusicNote,
                         contentDescription = null,
